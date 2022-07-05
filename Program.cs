8123ae30661db4fc392aba97f253909fad0c24f1
san27sin <sin27san@gmail.com>
2022-07-05 14:58:20 +0300

    Task#3  Работа со строками. Дан текстовый файл, содержащий ФИО и e-mail адрес. Разделителем между ФИО и адресом электронной почты является символ &: Кучма Андрей Витальевич & Kuchma@mail.ru Мизинцев Павел Николаевич & Pasha@mail.ru Сформировать новый файл, содержащий список адресов электронной почты. Предусмотреть метод, выделяющий из строки адрес почты. Методу в качестве параметра передается символьная строка s, e-mail возвращается в той же строке s: public void SearchMail (ref string s).

--- Program.cs
+++ Program.cs
@@ -1,25 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace objected_oriented_programming
 {
     internal class Program
     {
-        public static string Reverse (string str)
+        public static void SearchMail(ref string s)
         {
-            char[] letters = str.ToCharArray();
-            for(int begin = 0, end = letters.Length -1; begin < letters.Length/2; begin++,end--)
-            {
-                char letter = letters[begin];
-                letters[begin] = letters[end];
-                letters[end] = letter;
-            }
-            return new string(letters);
+            s = s.Substring(s.IndexOf('&') + 1).TrimStart(' ').TrimEnd(' ');
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Reverse("табуретка"));
-            Console.WriteLine(Reverse("москва"));
+            List<string> mails = new();
+            try
+            {
+                using (StreamReader sr = new StreamReader($"{Directory.GetCurrentDirectory()}\\name_mail.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        SearchMail(ref line);
+                        mails.Add(line);
+                    }
+                }
+
+                using (StreamWriter sw = File.CreateText($"{Directory.GetCurrentDirectory()}\\mails.txt"))
+                {
+                    foreach(string mail in mails)
+                    {
+                        sw.WriteLine(mail);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
