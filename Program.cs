900b4f13bcb48137f649413b99413d6f2ed384b8
san27sin <sin27san@gmail.com>
2022-07-05 12:04:11 +0300

    #Task2 Реализовать метод, который в качестве входного параметра принимает строку string, возвращает строку типа string, буквы в которой идут в обратном порядке. Протестировать метод.

--- Program.cs
+++ Program.cs
@@ -2,19 +2,24 @@
 
 namespace objected_oriented_programming
 {
-    class Program
+    internal class Program
     {
+        public static string Reverse (string str)
+        {
+            char[] letters = str.ToCharArray();
+            for(int begin = 0, end = letters.Length -1; begin < letters.Length/2; begin++,end--)
+            {
+                char letter = letters[begin];
+                letters[begin] = letters[end];
+                letters[end] = letter;
+            }
+            return new string(letters);
+        }
+
         static void Main(string[] args)
         {
-            BankAccount account_1 = new(10_000, BankAccount.typesOfBankAccounts.checking_account);
-            account_1.Print();
-
-            BankAccount account_2 = new(15_000, BankAccount.typesOfBankAccounts.checking_account);
-            account_2.Print();
-
-            account_1.TransferMoney(account_2, 2000);
-            account_1.Print();
-            account_2.Print();
+            Console.WriteLine(Reverse("табуретка"));
+            Console.WriteLine(Reverse("москва"));
         }
     }
 }
