67ab9b84f68932142b07b626c22600d41345ee6d
san27sin <sin27san@gmail.com>
2022-07-06 10:55:03 +0300

    Task#1 Реализовать класс для описания здания (уникальный номер здания, высота, этажность, количество квартир, подъездов). Поля сделать закрытыми, предусмотреть методы для заполнения полей и получения значений полей для печати. Добавить методы вычисления высоты этажа, количества квартир в подъезде, количества квартир на этаже и т.д. Предусмотреть возможность, чтобы уникальный номер здания генерировался программно. Для этого в классе предусмотреть статическое поле, которое бы хранило последний использованный номер здания, и предусмотреть метод, который увеличивал бы значение этого поля.

--- Program.cs
+++ Program.cs
@@ -1,61 +1,16 @@
 ﻿using System;
 
 namespace objected_oriented_programming
-{
-   abstract class MOSGORGEOTREST
-    {
-        string ROLE { get; set; }
-        public abstract void Act();
-    }
-
-    class CLient : MOSGORGEOTREST
-    {
-        
-       public override void Act()
-        {
-            Console.WriteLine("Делаю заказа");
-        }
-
-    }
-
-    class TreatySection : MOSGORGEOTREST
-    {
-        public override void Act()
-        {
-            Console.WriteLine("Заключаю договор");
-        }
-    }
-
-    class TaskDepartment : MOSGORGEOTREST
-    {
-        public override void Act()
-        {
-            Console.WriteLine("Подготавливаю план работ!");
-        }
-    }
-
-    class Laboratory : MOSGORGEOTREST
-    {
-        public override void Act()
-        {
-            Console.WriteLine("Делаю лабораторные испытания!");
-        }
-    }
-
-    class FieldWork : MOSGORGEOTREST
-    {
-        public override void Act()
-        {
-            Console.WriteLine("Отбор проб из скважины!");
-        }
-
-    }
-
+{  
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Building tower_1 = new(100, 500, 4);
+            tower_1.Print();
+
+            Building tower_2 = new(50, 200, 6);
+            tower_2.Print();
         }
     }
 }
